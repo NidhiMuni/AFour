86c4276d7befabfe0ffedc4bc5a93106eebf5cc8
Nidhi <nidhi.munikote@outlook.com>
2020-08-08 12:31:50 -0700

    Adding Quarterly Reviews API

--- employees/serializers.py
+++ employees/serializers.py
@@ -1,11 +1,56 @@
 from rest_framework import serializers
-from .models import Employee, Quarterly
+from .models import Employee, Quarterly, QuarterlyReview
 
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = ('id', 'first_name', 'last_name', 'manager_id', 'manager_name', 'du_head_id', 'du_head_name', 'date_of_joining', 'email', 'designation', 'service_status', 'project_id', 'project_name', 'project_start_date', 'project_end_date', 'coe_id', 'coe')
 
-class CoeSerializer(serializers.ModelSerializer):
+class QuarterlyReviewSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Employee
+        model = QuarterlyReview
+        fields = (
+            'id',
+            'qms_id', 
+            'employee_id', 
+            'manager_id', 
+            'manager_name', 
+            'du_head_id', 
+            'du_head_name',
+            'coe_head_id',
+            'coe_head_name',
+            'employee_service_status',
+            'manager_hard_skills_comments',
+            'manager_soft_skills_comments',
+            'manager_value_addition_comments',
+            'manager_up_learning_comments',
+            'manager_hard_skills_ratings',
+            'manager_soft_skills_ratings',
+            'manager_value_addition_ratings',
+            'manager_up_learning_ratings',
+            'goal',
+            'create_date',
+            'hr_form_submit_date',
+            'review_complete_date',
+            'last_modified_date',
+            'quarterly_rating',
+            'manager_form_submit_date', 
+            'workflow_status', 
+            'last_modified_by', 
+            'du_head_complete_date', 
+            'du_hard_skills_ratings', 
+            'du_soft_skills_ratings', 
+            'du_value_addition_ratings', 
+            'du_up_learning_ratings', 
+            'du_hard_skills_comments', 
+            'du_soft_skills_comments', 
+            'du_value_addition_comments', 
+            'du_up_learning_comments', 
+            'save_for_later', 
+            'first_name', 
+            'last_name', 
+            'manager_email', 
+            'du_head_email', 
+            'employee_email', 
+            'designation'
+            )
--- employees/views.py
+++ employees/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Employee
-from .serializers import EmployeeSerializer
+from .models import Employee, QuarterlyReview
+from .serializers import EmployeeSerializer, QuarterlyReviewSerializer
 from rest_framework import generics
 
 # Create your views here.
@@ -23,18 +23,21 @@ class EmployeeListCreate(generics.ListCreateAPIView):
             
         return queryset
 
-# class CoeListCreate(generics.ListCreateAPIView):
+class QuarterlyReviewListCreate(generics.ListCreateAPIView):
+    serializer_class = QuarterlyReviewSerializer
 
-#     class CoeClass:
-#         def __init__(self, coeId=None, coeName=None):
-#             self.coeName = coeName
-#             self.coeId = coeId
+    def get_queryset(self):
+        # retrieve all employee objects from DB
+        queryset = QuarterlyReview.objects.all()
 
-#     coeDictionary = dict()
+        # get coe_id and coe query params. If any one of them is present, filter
+        # the request would look like:
+        #       http://127.0.0.1:8000/api/employees?coe_id=AFTC0628
+        #       http://127.0.0.1:8000/api/employees?coe=COE%20TEst
+        queryParamEmployeeId = self.request.query_params.get('employee_id', None)
+        # queryParamCoe = self.request.query_params.get('coe', None)   
 
-#     allEmployees = Employee.objects.all()
-
-#     for employee in allEmployees:
-#         coeDictionary[employee.coe_id] = CoeClass(employee.coe_id, employee.coe)
-    
-
+        if queryParamEmployeeId is not None:
+           queryset = queryset.filter(employee_id=queryParamEmployeeId)
+            
+        return queryset
