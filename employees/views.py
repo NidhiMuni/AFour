744ad725daa1724aed1b1c1962ff2a1bf2117a60
Nidhi <nidhi.munikote@outlook.com>
2020-08-13 17:00:43 -0700

    Intermediate checkin
    Everything works except that PUT /api/employees/AFTD002 returns 404

--- employees/views.py
+++ employees/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import UpdateModelMixin
 from .models import Employee, QuarterlyReview
 from .serializers import EmployeeSerializer, QuarterlyReviewSerializer
 from rest_framework import generics
@@ -16,15 +18,13 @@ logger = logging.getLogger(__name__)
 
 #serializers.register_serializer('json', 'employees.serializers.EmployeeSerializer')
 
-class EmployeeListCreate(generics.ListCreateAPIView):
+class EmployeesView(generics.ListCreateAPIView):
     serializer_class = EmployeeSerializer
-    logger.warning(EmployeeSerializer)
 
     def get_queryset(self):
         # retrieve all employee objects from DB
         queryset = Employee.objects.all()
 
-        logger.warning('In EmployeeListCreate view!!')
         # get coe_id and coe query params. If any one of them is present, filter
         # the request would look like:
         #       http://127.0.0.1:8000/api/employees?coe_id=AFTC0628
@@ -37,32 +37,40 @@ class EmployeeListCreate(generics.ListCreateAPIView):
             
         return queryset
 
-class EmployeeView(generics.ListCreateAPIView):
-    serializer_class = EmployeeSerializer
-
-
-    @csrf_exempt #make the request exempt from providing CSRF cookie
-    def emp_by_id(request, empId):
-
-
-        if request.method == 'GET':
-            # get data for employee with id = empId 
-            employee = Employee.objects.get(id = empId)
-
-            # serialize the data
-            data = EmployeeSerializer(employee).data
-
-            #return the data as Json
-            return JsonResponse(data, safe=False)
-
-        elif request.method == 'PUT':
-            receivedPayload = json.loads(request.body.decode('utf-8'))
-            employee = Employee.objects.get(id = empId)
-            employee.update_fields(receivedPayload)
-            employee.save()
-            return HttpResponse(status=201)
-          
+class EmployeeView(GenericAPIView, UpdateModelMixin):
+    def get(self, request, *args, **kwargs):
+        # retrieve the empId variable from the URL
+        if (kwargs.get('empId') is not None):
+            empId = kwargs['empId']
+        else:
+            return HttpResponse('Invalid request - empId not found', statu = 400)
         
+        # get data for employee with id = empId 
+        self.queryset = Employee.objects.get(id = empId)
+        self.kwargs['pk'] = empId
+        
+        # get data for employee with id = empId 
+        employee = Employee.objects.get(id = empId)
+
+        # serialize the data
+        data = EmployeeSerializer(employee).data
+
+        #return the data as Json
+        return JsonResponse(data, safe=False)
+
+    def put(self, request, *args, **kwargs):
+        # retrieve the empId variable from the URL
+        if (kwargs.get('empId') is not None):
+            empId = kwargs['empId']
+        else:
+            return HttpResponse('Invalid request - empId not found', statu = 400)
+        
+        # get data for employee with id = empId 
+        self.queryset = Employee.objects.get(id = empId)
+        self.kwargs['pk'] = empId
+        
+        return self.partial_update(request, *args, **kwargs)
+
 class QuarterlyReviewListCreate(generics.ListCreateAPIView):
     serializer_class = QuarterlyReviewSerializer
 
